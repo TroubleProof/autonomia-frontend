f82078a3ea490912c86f0e13c142d287a13e9441
muriloignaccio <murilohsignacio@gmail.com>
2020-04-19 17:53:08 -0300

    estilizar cadastro

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -10,28 +10,33 @@ const Cadastro = ({navigation}) => {
     return(
         <>
             <View style={styles.container}>
+                <TouchableOpacity style={{alignSelf: "flex-start"}} onPress={() => navigation.goBack()}>
+                        <FontAwesome5 name='arrow-left' size={20}  color="#4C1B72"/>
+                </TouchableOpacity>
+
                 <View style={styles.header}>
-                <Image style={{width: 150, height: 150}} source={logo}/>
+                    <Image source={logo}/>
                 </View>
-                <ScrollView contentContainerStyle={styles.inputs}>
-                    <TextInput style={styles.input} placeholder='Nome completo'/>
-                    <TextInput style={styles.input} placeholder='Como quer ser chamado?'/>
-                    <TextInput style={styles.input} placeholder='Escolha seu gênero'/>
-                    <TextInput style={styles.input} placeholder='Data de nascimento'/>
-                    <TextInput style={styles.input} placeholder='CPF'/>
-                    <TextInput style={styles.input} placeholder='E-mail'/>
-                    <TextInput style={styles.input} placeholder='Telefone'/>
-                    <TextInput style={styles.input} placeholder='CEP'/>
-                    <TextInput style={styles.input} placeholder='Endereço'/>
-                    <TextInput style={styles.input} placeholder='Complemento'/>
-                    <TextInput style={styles.input} placeholder='Nº'/>
-                    <TextInput style={styles.input} placeholder='Bairro'/>
-                    <TextInput style={styles.input} placeholder='Cidade'/>
-                    <TextInput style={styles.input} placeholder='UF'/>
+
+                <ScrollView contentContainerStyle={styles.form} showsVerticalScrollIndicator={false}>
+                    <TextInput style={styles.form__input} placeholder='Nome completo'/>
+                    <TextInput style={styles.form__input} placeholder='Como quer ser chamado?'/>
+                    <TextInput style={styles.form__input} placeholder='Escolha seu gênero'/>
+                    <TextInput style={styles.form__input} placeholder='Data de nascimento'/>
+                    <TextInput style={styles.form__input} placeholder='CPF'/>
+                    <TextInput style={styles.form__input} placeholder='E-mail'/>
+                    <TextInput style={styles.form__input} placeholder='Telefone'/>
+                    <TextInput style={styles.form__input} placeholder='CEP'/>
+                    <TextInput style={styles.form__input} placeholder='Endereço'/>
+                    <TextInput style={styles.form__input} placeholder='Complemento'/>
+                    <TextInput style={styles.form__input} placeholder='Nº'/>
+                    <TextInput style={styles.form__input} placeholder='Bairro'/>
+                    <TextInput style={styles.form__input} placeholder='Cidade'/>
+                    <TextInput style={styles.form__input} placeholder='UF'/>
                     <Text>Comprovante de residência</Text>
-                    <TextInput style={styles.input} placeholder='Escolher arquivo...'/>
-                    <TextInput style={styles.input} placeholder='Criar Senha' secureTextEntry={true}/>
-                    <TextInput style={styles.input} placeholder='Digite novamente a senha' secureTextEntry={true}/>
+                    <TextInput style={styles.form__input} placeholder='Escolher arquivo...'/>
+                    <TextInput style={styles.form__input} placeholder='Criar Senha' secureTextEntry={true}/>
+                    <TextInput style={styles.form__input} placeholder='Digite novamente a senha' secureTextEntry={true}/>
                     <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
                         <Text style={{color: "white", fontWeight: "bold", fontSize: 18}}>Entrar</Text>
                     </TouchableOpacity>
